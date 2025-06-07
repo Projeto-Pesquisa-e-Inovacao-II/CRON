7a1c6438df5b0c2505813dcd0ddba0252f0e4430
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-06-07 13:43:13 -0300

    verificando usuarios com notificaoes ativadas

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -5,12 +5,18 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> mensagensSemanais = List.of(
-                "📌 Lembrete semanal: continue acompanhando as praças com maiores índices de evasão.",
+                "📌 17371 veículos evadiram no mês de maio na Ecovias Imigrantes",
                 "🔍 Dados constantes, evasões persistentes. Reveja os pontos críticos da semana.",
                 "📊 Reforce o monitoramento: as tendências semanais continuam estáveis ou mudaram?"
         );
 
         Telegram telegram = new Telegram(mensagensSemanais);
-        telegram.enviarMensagemAleatoria();
+//        Telegram telegram = new Telegram();
+
+        List<Long> usuariosNotificacoes = telegram.verificarNotificacoesAtivas();
+
+        telegram.enviarMensagemAleatoria(usuariosNotificacoes);
+
+
     }
 }
--- src/main/java/school/sptech/Telegram.java
+++ src/main/java/school/sptech/Telegram.java
@@ -1,6 +1,10 @@
 package school.sptech;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.json.JSONObject;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
@@ -9,7 +13,7 @@ import java.util.*;
 
 public class Telegram {
 
-    private final List<String> mensagens;
+    private List<String> mensagens = new ArrayList<>();
     private static final String TOKEN = "7753286594:AAHkbUrC_rgxK63_Qok9k0F2rCsXhdrUJxw";
     private static final String CHAT_ID = "7794980695";
     private static final String BASE_URL = "https://api.telegram.org/bot" + TOKEN + "/sendMessage";
@@ -20,27 +24,51 @@ public class Telegram {
         this.mensagens = mensagens;
     }
 
-    public void enviarMensagemAleatoria() {
-        String mensagem = mensagens.get(random.nextInt(mensagens.size()));
-        JSONObject payload = new JSONObject();
-        payload.put("chat_id", CHAT_ID);
-        payload.put("text", mensagem);
+    public Telegram() {
+    }
 
-        try {
-            HttpRequest request = HttpRequest.newBuilder(URI.create(BASE_URL))
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(payload.toString()))
-                    .build();
+    public void enviarMensagemAleatoria(List<Long> usuariosNotificacoes) {
+        for (Long usuario : usuariosNotificacoes) {
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                System.out.println("Mensagem enviada com sucesso!");
-            } else {
-                System.err.println("Erro ao enviar. Código: " + response.statusCode());
-                System.err.println("Resposta: " + response.body());
+            String mensagem = mensagens.get(random.nextInt(mensagens.size()));
+
+            System.out.println(usuario);
+            System.out.println(mensagem);
+
+            JSONObject payload = new JSONObject();
+            payload.put("chat_id", usuario);
+            payload.put("text", mensagem);
+
+            try {
+                HttpRequest request = HttpRequest.newBuilder(URI.create(BASE_URL))
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(payload.toString()))
+                        .build();
+
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200) {
+                    System.out.println("Mensagem enviada com sucesso!");
+                }
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
         }
+    }
+
+    public List<Long> verificarNotificacoesAtivas() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/dataway");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("123456");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(basicDataSource);
+
+        List<Long> verificacaoUsuarioNotificacaoAtiva = jdbcTemplate.query(
+                "SELECT telegramChatId FROM Usuario " +
+                        "JOIN Empresa ON Usuario.fkEmpresa = Empresa.idEmpresa " +
+                        "Where Empresa.ativo = 1 AND usuario.notificacoesAtivas = 1;", (rs, rowNum) -> rs.getLong("telegramChatId"));
+
+        return verificacaoUsuarioNotificacaoAtiva;
     }
 }
